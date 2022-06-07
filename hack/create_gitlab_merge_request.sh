69b5b6d33620c261caa38e2eed29d466e7df5e17
Matthew Wong <mattwon@amazon.com>
2022-06-07 13:08:14 -0700

    Add create_gitlab_merge_request for creating GIT_TAG MRs

---
+++ hack/create_gitlab_merge_request.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+source "$REPO_ROOT/hack/lib/patch.sh"
+
+set -e
+set -u
+
+PARENT_PATCHES_DIR=$(realpath "$1")
+MINOR_VERSION="$2"
+
+if git diff --exit-code "$PARENT_PATCHES_DIR"/GIT_TAG; then
+  echo "No diff to merge"
+  exit 0
+fi
+
+LATEST_VERSION=$(get_patches_base_version "$PARENT_PATCHES_DIR")
+MESSAGE="Update $MINOR_VERSION patches base version to $LATEST_VERSION"
+git checkout -b update_patches_base_version_"$MINOR_VERSION"
+git add "$PARENT_PATCHES_DIR"/GIT_TAG
+git commit -m "$MESSAGE"
+git pull --squash --rebase origin update_patches_base_version_"$MINOR_VERSION" || true
+git push -o merge_request.create -o merge_request.merge_when_pipeline_succeeds -o merge_request.remove_source_branch https://gitlab-ci-token:"$PROJECT_ACCESS_TOKEN"@gitlab.aws.dev/mattwon/ekskubernetespatches.git update_patches_base_version_"$MINOR_VERSION"
+
+git checkout mainline
+git branch -D update_patches_base_version_"$MINOR_VERSION"
